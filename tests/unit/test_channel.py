f82968958e7cecb68d35da414faf003b834b4c7f
Aliaksei Tarasenka <aliaksei.tarasenka@aegeurope.com>
2024-05-24 17:56:23 +0200

    replace directory, add unit tests

---
+++ tests/unit/test_channel.py
@@ -0,0 +1,36 @@
+# import unittest to use its   functionality.
+import unittest
+import xml.etree.ElementTree as ET
+from rss_reader.channel import XMLChannel
+
+
+class TestXMLChannel(unittest.TestCase):
+    def test_get_channel_xml(self):
+        xml_string = """<channel>
+                            <title>Test Channel</title>
+                            <link>http://example.com</link>
+                            <lastBuildDate>Mon, 20 Sep 2021 10:37:00 GMT</lastBuildDate>
+                            <pubDate>Mon, 20 Sep 2021 10:37:00 GMT</pubDate>
+                            <language>en-us</language>
+                            <category>Test</category>
+                            <managingEditor>Editor</managingEditor>
+                            <description>This is a test channel</description>
+                        </channel>"""
+        xml_element = ET.fromstring(xml_string)
+        expected_result = {
+            'feed': 'Test Channel',
+            'link': 'http://example.com',
+            'lastBuildDate': 'Mon, 20 Sep 2021 10:37:00 GMT',
+            'pubDate': 'Mon, 20 Sep 2021 10:37:00 GMT',
+            'language': 'en-us',
+            'category': 'Test',
+            'managingEditor': 'Editor',
+            'description': 'This is a test channel'
+        }
+
+        result = XMLChannel.get_channel_xml(xml_element)
+        self.assertDictEqual(result, expected_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/unit/test_item.py
@@ -0,0 +1,51 @@
+import unittest
+import xml.etree.ElementTree as ET
+from rss_reader.item import XMLItem
+
+
+class TestXMLItem(unittest.TestCase):
+    def test_get_items_xml(self):
+        xml_string = """<channel>
+                            <item>
+                                <title>Test Item 1</title>
+                                <author>Author 1</author>
+                                <pubDate>Tue, 21 Sep 2021 14:37:00 GMT</pubDate>
+                                <link>http://example.com/1</link>
+                                <category>Category 1</category>
+                                <description>This is a test item 1</description>
+                            </item>
+                            <item>
+                                <title>Test Item 2</title>
+                                <author>Author 2</author>
+                                <pubDate>Tue, 21 Sep 2021 15:37:00 GMT</pubDate>
+                                <link>http://example.com/2</link>
+                                <category>Category 2</category>
+                                <description>This is a test item 2</description>
+                            </item>
+                        </channel>"""
+        xml_element = ET.fromstring(xml_string)
+        expected_result = {"items": [
+            {
+                'title': 'Test Item 1',
+                'author': 'Author 1',
+                'pubDate': 'Tue, 21 Sep 2021 14:37:00 GMT',
+                'link': 'http://example.com/1',
+                'category': 'Category 1',
+                'description': 'This is a test item 1'
+            },
+            {
+                'title': 'Test Item 2',
+                'author': 'Author 2',
+                'pubDate': 'Tue, 21 Sep 2021 15:37:00 GMT',
+                'link': 'http://example.com/2',
+                'category': 'Category 2',
+                'description': 'This is a test item 2'
+            }
+        ]}
+
+        result = XMLItem.get_items_xml(xml_element, 2)
+        self.assertDictEqual(result, expected_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
